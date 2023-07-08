964094c78a1698922c19f51131f939b74d2d5b3c
Nmbrthirteen <nikushasiradzee@gmail.com>
2023-07-08 20:48:52 +0400

    2.2

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ defined( 'ABSPATH' ) || exit;
 
 function custom_activity_options_scripts() {
     wp_enqueue_style( 'custom-activity-options-style', plugin_dir_url( __FILE__ ) . '/main.css' );
-    wp_enqueue_script( 'custom-activity-options', plugin_dir_url( __FILE__ ) . 'custom-activity-options.js', array( 'jquery' ), null, true );
+    wp_enqueue_script( 'custom-activity-options', plugin_dir_url( __FILE__ ) . 'custom-activity-options.js', array( 'jquery' ), '1.0.0', true);
     wp_localize_script( 'custom-activity-options', 'custom_activity_options', array(
         'activity_permalink' => trailingslashit( bp_get_root_domain() . '/' . bp_get_activity_root_slug() )
     ) );
 }
-add_action( 'wp_enqueue_scripts', 'custom_activity_options_scripts' );
+add_action( 'wp_enqueue_scripts', 'custom_activity_options_scripts', 10 );
 

13f8969b98a294e181701b13bd26aad3f071ec73
Nika Siradze <61732491+nmbrthirteen@users.noreply.github.com>
2024-01-06 17:29:34 +0400

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ function custom_activity_options_scripts() {
 add_action('wp_enqueue_scripts', 'custom_activity_options_scripts', 10);
 
 function check_buddyboss_platform() {
-    if ( ! class_exists('BuddyBoss') || ! function_exists('bp_is_active') || ! bp_is_active('activity') ) {
+    if (!class_exists('BuddyBoss') || !function_exists('bp_is_active') || !bp_is_active('activity')) {
         // BuddyBoss Platform or Activity Feeds is not active
         deactivate_plugins(plugin_basename(__FILE__));
         add_action('admin_notices', 'custom_activity_options_admin_notice');
@@ -30,7 +30,8 @@ register_activation_hook(__FILE__, 'check_buddyboss_platform');
 function custom_activity_options_admin_notice() {
     ?>
     <div class="notice notice-error is-dismissible">
-        <p><?php _e('Copy Activity Link requires BuddyBoss Platform and the Activity Feeds component to be active. Please make sure they are installed and activated before activating this plugin.', 'copy-activity-link'); ?></p>
+        <p><?php esc_html_e('Copy Activity Link requires BuddyBoss Platform and the Activity Feeds component to be active. Please make sure they are installed and activated before activating this plugin.', 'copy-activity-link'); ?></p>
     </div>
     <?php
 }
+add_action('admin_notices', 'custom_activity_options_admin_notice');

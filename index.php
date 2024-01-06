10131e574d89779846059956f9fa0c712bd29d88
Nika Siradze <61732491+nmbrthirteen@users.noreply.github.com>
2024-01-06 17:18:03 +0400

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,11 @@
  	Author URI: https://github.com/nmbrthirteen/
 */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 function custom_activity_options_scripts() {
-    wp_enqueue_style( 'custom-activity-options-style', plugin_dir_url( __FILE__ ) . '/main.css' );
-    wp_enqueue_script( 'custom-activity-options', plugin_dir_url( __FILE__ ) . 'custom-activity-options.js', array( 'jquery' ), '1.0.0', true);
-    wp_localize_script( 'custom-activity-options', 'custom_activity_options', array(
-        'activity_permalink' => trailingslashit( bp_get_root_domain() . '/' . bp_get_activity_root_slug() )
-    ) );
+    wp_enqueue_style('custom-activity-options-style', plugin_dir_url(__FILE__) . '/main.css');
+    wp_enqueue_script('custom-activity-options', plugin_dir_url(__FILE__) . 'custom-activity-options.js', array('jquery'), '1.0.0', true);
+    wp_add_inline_script('custom-activity-options', 'var custom_activity_options = ' . wp_json_encode(array('activity_permalink' => trailingslashit(bp_get_root_domain() . '/' . bp_get_activity_root_slug()))), 'before');
 }
-add_action( 'wp_enqueue_scripts', 'custom_activity_options_scripts', 10 );
-
+add_action('wp_enqueue_scripts', 'custom_activity_options_scripts', 10);
